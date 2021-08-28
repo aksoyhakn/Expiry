1de70b501a99f7f56237dce0a1181dc50d10cae3
Hakan Aksoy <hakan.aksoy@vakifbank.com.tr>
2021-08-28 17:54:51 +0300

    added image

--- app/src/main/java/com/iyiyasa/android/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/iyiyasa/android/ui/home/HomeViewModel.kt
@@ -5,7 +5,6 @@ import androidx.hilt.lifecycle.ViewModelInject
 import com.iyiyasa.android.base.viewmodel.BaseViewModel
 import com.iyiyasa.android.data.persistence.AppDatabase
 import com.iyiyasa.android.data.persistence.entity.Data
-import com.iyiyasa.android.ui.barcode.model.BarcodeResponse
 import java.util.*
 import kotlin.random.Random
 
@@ -22,8 +21,9 @@ class HomeViewModel @ViewModelInject constructor(
 
     var item = ObservableField<ArrayList<Data>>()
 
-    fun add(){
-        appDatabase.iyiyasaDAO().insertData(Data(null, Random.nextInt().toString(),"5449000011114","COCA COLA 200ML"))
+    fun add() {
+        appDatabase.iyiyasaDAO()
+            .insertData(Data(null, Random.nextInt().toString(), "5449000011114", "COCA COLA 200ML"))
         item.set(appDatabase.iyiyasaDAO().getData() as ArrayList<Data>)
     }
 
